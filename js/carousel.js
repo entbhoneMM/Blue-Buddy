a62593335367347f070b01d12b6db75593835a80
entbhone <entbhonemyintmo@gmail.com>
2022-09-10 21:10:15 +0630

    portfolio done

--- js/carousel.js
+++ js/carousel.js
@@ -1,43 +1,47 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin: 30,
-    
-    nav:true,
-    navText: ["Prev","Next"],
-
-    
-    dots:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:4
-        }
-    }
-})
-
-//  use this code if upper code encounter with some issues
-// $(document).ready(function () {
-//     $(".owl-carousel").owlCarousel();
-// });
-
 // $('.owl-carousel').owlCarousel({
-//     loop: true,
-//     margin: 10,
-//     nav: true,
-//     responsive: {
-//         0: {
-//             items: 1
+//     loop:true,
+//     margin: 30,
+    
+//     nav:true,
+//     navText: ["Prev","Next"],
+
+//     dots:false,
+
+//     responsive:{
+//         0:{
+//             items:1
 //         },
-//         600: {
-//             items: 3
+//         600:{
+//             items:3
 //         },
-//         1000: {
-//             items: 5
+//         1000:{
+//             items:4
 //         }
 //     }
 // })
+
+//  use this code if upper code encounter with some issues
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    navText: ["Prev","Next"],
+    
+
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+})
