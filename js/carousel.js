bbb1361de2405f921d58fd5ef6692e5dec3138cc
entbhone <entbhonemyintmo@gmail.com>
2022-09-10 21:47:36 +0630

    portfolio mobile error fixed

--- js/carousel.js
+++ js/carousel.js
@@ -1,26 +1,4 @@
-// $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin: 30,
-    
-//     nav:true,
-//     navText: ["Prev","Next"],
 
-//     dots:false,
-
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:4
-//         }
-//     }
-// })
-
-//  use this code if upper code encounter with some issues
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel();
 });
@@ -47,3 +25,27 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+//  use this code if upper code encounter with some issues
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin: 30,
+    
+//     nav:true,
+//     navText: ["Prev","Next"],
+
+//     dots:false,
+
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:4
+//         }
+//     }
+// })
