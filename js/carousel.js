6c1787f50761972323f954f2cf5e684e6ce1e9c9
entbhone <entbhonemyintmo@gmail.com>
2022-09-10 18:00:46 +0630

    portfolio done

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,12 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
+    margin: 30,
+    
     nav:true,
+    navText: ["Prev","Next"],
+
+    
+    dots:false,
     responsive:{
         0:{
             items:1
@@ -10,7 +15,29 @@ $('.owl-carousel').owlCarousel({
             items:3
         },
         1000:{
-            items:5
+            items:4
         }
     }
 })
+
+//  use this code if upper code encounter with some issues
+// $(document).ready(function () {
+//     $(".owl-carousel").owlCarousel();
+// });
+
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: true,
+//     responsive: {
+//         0: {
+//             items: 1
+//         },
+//         600: {
+//             items: 3
+//         },
+//         1000: {
+//             items: 5
+//         }
+//     }
+// })
---
+++ js/toggle.js
@@ -0,0 +1,6 @@
+let toggleBtn = document.querySelector('.toggle');
+let ul = document.querySelector('ul');
+        
+    toggleBtn.onclick = function() {
+       ul.classList.toggle('active');
+    }   
